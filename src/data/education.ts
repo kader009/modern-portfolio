b3fd609e3d269948ab03c017961c3ff91c155ee2
kader009 <kadermollarasel000@gmail.com>
2025-05-24 16:34:01 +0600

    education added

--- src/data/education.ts
+++ src/data/education.ts
@@ -2,36 +2,36 @@ import { type ExperienceShowcaseListItemProps } from "@/components/experience/ex
 
 export const EDUCATION: ExperienceShowcaseListItemProps[] = [
   {
-    title: "Bachelor Of Engineering in Electronics and Telecommunication",
+    title: "Bachelor Of Arts",
     organisation: {
-      name: "Rizvi College Of Engineering",
-      href: "https://eng.rizvi.edu.in/",
+      name: "Bhawal Badre Alam Govt. College",
+      href: "https://bbagc.edu.bd/",
     },
-    date: "2019-2023",
-    location: "Bandra, Mumbai",
+    date: "2022 - Present",
+    location: "Gazipur Chowrasta, Gazipur",
     description:
-      "Relevant courses included Data Structures and Algorithms, Web Design, Cloud Computing, Artificial Neural Network and Database Management.",
+      "Relevant courses included Critical Thinking and Logic, Research Methodologies, History of Scientific Ideas, English Composition and Communication, and Philosophical Foundations of Knowledge.",
   },
   {
-    title: "Higher Secondary School",
+    title: "Higher Secondary Education",
     organisation: {
-      name: "Mithibai College",
-      href: "https://www.mithibai.ac.in/",
+      name: "Momtaz Uddin Business Management College",
+      href: "https://www.facebook.com/mbmc.edu.bd",
     },
-    date: "2016-2018",
-    location: "Vile Parle, Mumbai",
+    date: "2018 - 2020",
+    location: "Abdullahpur, Uttara, Dhaka",
     description:
-      "Completed the course in Electronics Bifocal Science with 68%. Started freelancing making 3D assets for games. Got interested in game development and learned unity and c#",
+      "I completed my Higher Secondary Education (HSC) in Business Management from Mumtaz Uddin Business Management College with core subjects in Bangla, English, Mathematics, and ICT.",
   },
   {
     title: "Secondary School Education",
     organisation: {
-      name: "Holy Family High School",
-      href: "https://holyfamilyandheri.org/",
+      name: "Birampur Pilot High School",
+      href: "Birampur Pilot High School",
     },
-    date: "2017-2018",
-    location: "Andheri, Mumbai",
+    date: "2008 - 2013",
+    location: "Notun Bazar, Birampur",
     description:
-      "Learned good values,dedication and disciplne. Played district football for 5 years.",
+      "Learned good values, dedication and disciplne. Played district Cricket for 5 years.",
   },
 ];
